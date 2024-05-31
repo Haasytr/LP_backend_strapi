2277b2e970e9c8a30cebe08698abf9bc252d7072
Vinicius Oliveira <viniciuszero@Live.com>
2024-05-31 17:20:33 -0300

    First commit

---
+++ types/generated/components.d.ts
@@ -0,0 +1,146 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface MenuMenuLinks extends Schema.Component {
+  collectionName: 'components_menu_menu_links';
+  info: {
+    displayName: 'menu_links';
+    icon: 'alien';
+  };
+  attributes: {
+    link_text: Attribute.String & Attribute.Required;
+    url: Attribute.Text & Attribute.Required;
+    open_in_new_tab: Attribute.Boolean &
+      Attribute.Required &
+      Attribute.DefaultTo<false>;
+  };
+}
+
+export interface MenuMenu extends Schema.Component {
+  collectionName: 'components_menu_menus';
+  info: {
+    displayName: 'menu';
+    icon: 'book';
+    description: '';
+  };
+  attributes: {
+    logo_link: Attribute.Text & Attribute.Required;
+    logo_text: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        minLength: 3;
+        maxLength: 255;
+      }>;
+    logo: Attribute.Media<'images', true>;
+    menu_links: Attribute.Component<'menu.menu-links', true> &
+      Attribute.Required;
+  };
+}
+
+export interface SectionImageGrid extends Schema.Component {
+  collectionName: 'components_section_image_grids';
+  info: {
+    displayName: 'image_grid';
+    icon: 'apps';
+  };
+  attributes: {
+    image: Attribute.Media<'images', true>;
+  };
+}
+
+export interface SectionSectionContent extends Schema.Component {
+  collectionName: 'components_section_section_contents';
+  info: {
+    displayName: 'section_content';
+    icon: 'collapse';
+  };
+  attributes: {
+    title: Attribute.String & Attribute.Required;
+    content: Attribute.Blocks & Attribute.Required;
+    metadata: Attribute.Component<'section.section-metadata'> &
+      Attribute.Required;
+  };
+}
+
+export interface SectionSectionGrid extends Schema.Component {
+  collectionName: 'components_section_section_grids';
+  info: {
+    displayName: 'section_grid';
+    icon: 'emotionUnhappy';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String & Attribute.Required;
+    description: Attribute.Text & Attribute.Required;
+    text_grid: Attribute.Component<'section.text-grid', true>;
+    image_grid: Attribute.Component<'section.image-grid', true>;
+    metadata: Attribute.Component<'section.section-metadata'>;
+  };
+}
+
+export interface SectionSectionMetadata extends Schema.Component {
+  collectionName: 'components_section_section_metadata';
+  info: {
+    displayName: 'section_metadata';
+    icon: 'medium';
+  };
+  attributes: {
+    name: Attribute.String & Attribute.Required;
+    section_id: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        minLength: 3;
+        maxLength: 50;
+      }>;
+    background: Attribute.Boolean &
+      Attribute.Required &
+      Attribute.DefaultTo<false>;
+  };
+}
+
+export interface SectionSectionTwoColumns extends Schema.Component {
+  collectionName: 'components_section_section_two_columns';
+  info: {
+    displayName: 'section_two_columns';
+    icon: 'bulletList';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        minLength: 3;
+        maxLength: 255;
+      }>;
+    description: Attribute.Text & Attribute.Required;
+    image: Attribute.Media<'images'> & Attribute.Private;
+    metadata: Attribute.Component<'section.section-metadata'> &
+      Attribute.Required;
+  };
+}
+
+export interface SectionTextGrid extends Schema.Component {
+  collectionName: 'components_section_text_grids';
+  info: {
+    displayName: 'text_grid';
+    icon: 'alien';
+  };
+  attributes: {
+    title: Attribute.String & Attribute.Required;
+    description: Attribute.String & Attribute.Required;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'menu.menu-links': MenuMenuLinks;
+      'menu.menu': MenuMenu;
+      'section.image-grid': SectionImageGrid;
+      'section.section-content': SectionSectionContent;
+      'section.section-grid': SectionSectionGrid;
+      'section.section-metadata': SectionSectionMetadata;
+      'section.section-two-columns': SectionSectionTwoColumns;
+      'section.text-grid': SectionTextGrid;
+    }
+  }
+}
